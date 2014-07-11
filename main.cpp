fdd20ec64bf8b0df856600ef23a36b25a1518172
Ondrej Slamecka <ondrej@slamecka.cz>
2014-07-11 18:24:48 +0200

    Started with Finding D (line 4)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
-#include <vector>
+#include <algorithm>
+#include <set>
 #include <ogdf/basic/Graph.h>
 #include <ogdf/basic/graph_generators.h>
 #include <ogdf/layered/DfsAcyclicSubgraph.h>
+#include <ogdf/basic/Queue.h>
 
 using namespace std;
 using namespace ogdf;
+
+int m = 3; // Cocircuit size bound
 
 /**
  * @brief Takes a csv file with lines "<id>;<source>;<target>;<edge name>;..." and transforms it into graph
@@ -44,10 +48,63 @@ Graph csvToGraph(string sEdgesFileName) { // This copies Graph on exit, to speed
             nodes[v] = G.newNode(v);
 
         G.newEdge(nodes[u], nodes[v], id);
+        G.newEdge(nodes[v], nodes[u], id);
     }
 
     return G;
 }
+
+/**
+ * Performs BFS to find the shortest path from s to t without using any edge from forbidden edges.
+ * Returns empty set if no such path exists.
+ */
+Array<edge> shortestPath(const Graph &g, node s, node t, List<edge> forbidden) {
+    Array<edge> path;
+    node v, u;
+    edge e;
+
+    Queue<node> Q;
+
+    NodeArray<bool> visited(g);
+    forall_nodes(v, g) visited[v] = false;
+
+    Q.append(s);
+    visited[s] = true;
+
+    while(!Q.empty()) {
+        v = Q.pop();
+
+        if (v == t) {
+            // presypej visited do path a vrat path
+        }
+
+        forall_adj_edges(e, v) {
+            if (e in forbidden) continue; // TODO: Use BST to represent forbidden (/sort forbidden and use binary search to look for e)
+
+            u = e->target();
+            if (!visited[u]) {
+                visited[u] = true;
+                Q.append(u);
+            }
+        }
+    }
+
+    return path;
+}
+
+set<edge> GenCocircuits(const Graph &g, set<edge> X, NodeArray<int> coloring) {
+    // if(E\X contains no hyperplane of M || X.size() > m)
+        // return NULL;
+
+    // Find set D = (a short circuit C in G, s. t. |C ∩ X| = 1) \ X
+    Array<edge> D = shortestPath(g, s, t, X);
+    if (D.size() > 0) { // can't use size
+        // for each c ∈ D, recursively call GenCocircuits(X ∪ {c}).
+    } else {
+        // If there is no such circuit C above (line 4), then return ‘Cocircuit: X’.
+    }
+}
+
 
 int main()
 {
@@ -56,13 +113,20 @@ int main()
     try {
         Graph g = csvToGraph("data/silnice.csv");
         List<edge> base;
+        NodeArray<int> coloring(g);
 
-        DfsAcyclicSubgraph.call(g, base); // Line 1
+        DfsAcyclicSubgraph.call(g, base); // Line 1        
+
+        for(List<edge>::iterator i = base.begin(); i != base.end(); i++) {
+            set<edge> X; // (Indexes might be sufficient? Check later)
+            X.insert(*i);
+            coloring[(*i)->source()] = 0;
+            coloring[(*i)->target()] = 1;
+            GenCocircuits(g, X, coloring);
+        }
 
 
 
-
-        //delete g;
     } catch (invalid_argument *e) {
         cerr << "Error: " << e->what() << endl;
         return 1;
