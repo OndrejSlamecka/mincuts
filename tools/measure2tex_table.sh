93acd5c3b0b5b1202c2b7fc83eccc4effea97de4
Ondrej Slamecka <ondrej@slamecka.cz>
2015-05-04 21:42:54 +0200

    measure2tex tool modified to use with measure_noncanonicity

--- tools/measure2tex_table.sh
+++ tools/measure2tex_table.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 0 ]] && [[ $1 != "-c" ]]; then
-	echo "Input: A file produced by measure_on_graph.sh tool, add -c switch " \
-		 "to output number of bonds produced instead of time taken"
+if [[ $# -ne 1 ]] || [[ $1 == "-h" ]]; then
+	echo "-t, -c -- input: a file produced by measure_on_graph.sh tool"
+	echo "-t     -- table with running time"
+	echo "-c     -- table with # of produced bonds"
+	echo "-n     -- input: a file produced by measure_noncanonicity.sh tool"
+	echo "       -- table with <(# of noncan.) / (# of can.) * 100> '%'"
 	echo "Output: TeX table of the result"
 	exit 1
 fi
 
 argv0=$1
+
+# http://askubuntu.com/questions/179898/how-to-round-decimals-using-bc-in-bash
+round()
+{
+	echo $(env printf %.$2f $(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc))
+};
 
 # Process the input
 components=2
@@ -16,17 +25,27 @@ declare -A table
 
 while read line
 do
-	# Save the biggest # of components and # of edges
 	if [[ "$line" != "#"* && "$line" != "" ]]; then
 		split_line=($line)
 		c=${split_line[0]}
 		e=${split_line[1]}
-		if [[ $argv0 == "-c" ]]; then
-			table[$c,$e]=${split_line[3]}
-		else
+
+		# Process according to use case
+		if [[ $argv0 == "-t" ]]; then
 			table[$c,$e]=${split_line[2]}
 		fi
 
+		if [[ $argv0 == "-c" ]]; then
+			table[$c,$e]=${split_line[3]}
+		fi
+
+		if [[ $argv0 == "-n" ]]; then
+			r=$(echo "${split_line[2]} / ${split_line[3]} * 100 - 100" | bc -l)
+			r=$(round "$r" 3)
+			table[$c,$e]=$r
+		fi
+
+		# Save the biggest # of components and # of edges
 		if [[ $c -gt "$components" ]]; then
 			components=$c
 		fi
