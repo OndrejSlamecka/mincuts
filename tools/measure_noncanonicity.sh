b4f1b32d0cda1d0df8117d597e10a6f9a19e8abf
Ondrej Slamecka <ondrej@slamecka.cz>
2015-05-10 00:49:12 +0200

    measure_noncanonicity: accept range as input

--- tools/measure_noncanonicity.sh
+++ tools/measure_noncanonicity.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
 if [[ $# -ne 3 ]]; then
-	echo "Usage: $0 <edge file.csv> <max cut size bound> <max # of component>"
+	echo "Usage: $0 <edge file.csv> <min>-<max cut size bound> <min>-<max # of components>"
 	exit 1
 fi
 
-min_c=3
 min_e=3
-
 max_e=$2
+if [[ "$2" == *"-"* ]]; then
+	parts=(${2//-/ })
+	min_e=${parts[0]}
+	max_e=${parts[1]}
+fi
+
+min_c=3
 max_c=$3
+if [[ "$3" == *"-"* ]]; then
+	parts=(${3//-/ })
+	min_c=${parts[0]}
+	max_c=${parts[1]}
+fi
 
 storage_file=tmp/measure_noncan_storage_$(basename $1)
 
